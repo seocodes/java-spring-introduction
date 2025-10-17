485555d20eb7ae67f14c68db8fdd86b14a9ad859
seocodes <azanettev@gmail.com>
2025-10-17 16:48:32 -0300

    iniciozinho
    Signed-off-by: seocodes <azanettev@gmail.com>

---
+++ src/main/java/br/com/seocodes/task_manager/user/UserController.java
@@ -0,0 +1,29 @@
+package br.com.seocodes.task_manager.user;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+// controller = componente que vai ser uma camada entre a requisição e as demais camadas (service, repository etc)
+// recebe a requisição do usuário - primeira camada de acesso
+public class UserController {
+
+    /*
+    * MÉTODOS DE ACESSO DO HTTP
+    * GET - buscar um dado/informação
+    * POST - adicionar um dado/informação
+    * PUT - alterar um dado/informação
+    * DELETE - remover um dado/informação
+    * PATCH - alterar somente uma parte da info/dado
+    */
+
+    // POST indiica que o navegador tá enviando dados pro servidor
+    // nesse caso os dados serão passados no corpo da requisição (como visto abaixo)
+    @PostMapping("/")
+    // @RequestBody para indiicar que o parâmetro (UserModel user) vai ser preenchido com os dados que vem no corpo da req
+    // O Spring converte o corpo da requisição (normalmente JSON) em um objeto UserModel
+    public void create(@RequestBody UserModel user){
+        System.out.println(user.getName());
+    }
+}
---
+++ src/main/java/br/com/seocodes/task_manager/user/UserModel.java
@@ -0,0 +1,13 @@
+package br.com.seocodes.task_manager.user;
+
+import lombok.Data;
+
+//Getters e Setters de tudo são feitos automaticamente (Lombok)
+@Data
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+}
+
+
