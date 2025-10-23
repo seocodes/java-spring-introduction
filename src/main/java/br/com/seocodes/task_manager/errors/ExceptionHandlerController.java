4eb2c5ad2482fba55ddafd79c5d4231878f134e0
seocodes <azanettev@gmail.com>
2025-10-23 16:29:20 -0300

    .

--- src/main/java/br/com/seocodes/task_manager/errors/ExceptionHandlerController.java
+++ src/main/java/br/com/seocodes/task_manager/errors/ExceptionHandlerController.java
@@ -6,13 +6,14 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-// Customização de erros
-@ControllerAdvice  // anotação para definir classes globais no momento de tratamento de exceções
-// toda exceção que ocorrer vai passar por aqui, e se a exceção for de um tipo que tratamos aqui, fará a ação que declararmos
+// Essa classe serve principalmente para customização de retorno para exceções
+
+@ControllerAdvice  // anotação para definir classes globais que atuam no momento de tratamento de exceções
+// toda exceção que ocorrer vai passar por aqui, e se a exceção for de um tipo que tratarmos aqui, fará a ação que declararmos
 public class ExceptionHandlerController {
     // pra interceptar a exceção de tipo HttpMessageNotReadable e retornar uma resposta personalizada
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMostSpecificCause().getMessage());
     }
 }
